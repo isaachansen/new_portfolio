582ba80c0ee871658fb530e20748729e402f9bed
Isaac Hansen <isaac@Isaacs-MacBook-Pro-2.local>
2019-12-05 14:14:56 -0700

    added chat-tier

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { TiArrowLeftThick } from "react-icons/ti";
 import { NavLink } from "react-router-dom";
 import qndp from "./qndp_img.jpg";
-import sneaks from "./sneaks_img.jpg"
+import sneaks from "./sneaks_img.jpg";
+import chattier from "./chat-tier.jpg";
 import "./Projects.scss";
 
 export default class Projects extends Component {
@@ -15,11 +16,14 @@ export default class Projects extends Component {
         <div className="project-grid">
           <div className="project-1">
             <div className="img-loc">
-              <img className="qndp-img" src={qndp} alt="qndp" />
+              <img className="img" src={qndp} alt="qndp" />
             </div>
             <div className="paragraph">
               <h1 className="proj-title">QNDP</h1>
-              <h3 className="proj-text">* NOT RELEASED YET * <br></br>  A minimalistic static site used by QNDP, a parent company for smaller businesses.</h3>
+              <h3 className="proj-text">
+                * NOT RELEASED YET * <br></br> A minimalistic static site used
+                by QNDP, a parent company for smaller businesses.
+              </h3>
               <h3 className="proj-text">Created with React</h3>
             </div>
             <div className="pro-links">
@@ -31,12 +35,18 @@ export default class Projects extends Component {
           </div>
           <div className="project-2">
             <div className="img-loc">
-              <img className="sneaks-img" src={sneaks} alt="sneaks" />
+              <img className="img" src={sneaks} alt="sneaks" />
             </div>
             <div className="paragraph">
               <h1 className="proj-title">SNEAKS</h1>
-              <h3 className="proj-text">A fullstack E-Commerce website. Features include Stripe checkout, account creation and fully functional cart.</h3>
-              <h3 className="proj-text">Created with React, React Redux, Node.js, Express, Massive, BCrypt and SASS</h3>
+              <h3 className="proj-text">
+                A fullstack E-Commerce website. Features include Stripe
+                checkout, account creation and fully functional cart.
+              </h3>
+              <h3 className="proj-text">
+                Created with React, React Redux, Node.js, Express, Massive,
+                BCrypt and SASS
+              </h3>
             </div>
             <div className="pro-links">
               <a href="https://github.com/isaachansen/sneaks">
@@ -48,13 +58,24 @@ export default class Projects extends Component {
             </div>
           </div>
           <div className="project-3">
-            <div className="img-loc"></div>
+          <div className="img-loc">
+              <img className="img" src={chattier} alt="chattier" />
+            </div>
             <div className="paragraph">
-              <h3 className="proj-text">GROUP-PROJECT | WORK IN-PROGRESS</h3>
+            <h1 className="proj-title">CHAT-TIER</h1>
+              <h3 className="proj-text">
+                * NOT HOSTED YET * <br></br>
+                Full-stack live chat application built with the purpose of learning Socket.io
+              </h3>
+              <h3 className="proj-text">
+                Created with React, Node.js, Express, Socket.io and SASS
+              </h3>
             </div>
             <div className="pro-links">
-              {/* <button className="proj-btn">VIEW CODE</button>
-              <button className="proj-btn">LIVE SITE</button> */}
+            <a href="https://github.com/isaachansen/Chat-tier">
+                <button className="proj-btn">VIEW CODE</button>
+              </a>
+              {/* <button className="proj-btn">LIVE SITE</button> */}
             </div>
           </div>
         </div>
